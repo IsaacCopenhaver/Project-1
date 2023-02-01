76b37839d1eae4dd0e192acc3d20760b7dc32594
Isaac Copenhaver <isaaccopenhaver@gmail.com>
2023-02-01 15:07:47 -0500

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,34 +1,53 @@
 import java.util.Vector;
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.io.File;
 public class Main{
     public static void main(String [] args){
-        ArrayList<Integer> List = new ArrayList<>();
-        Integer[] Array = new Integer[50000000];
-        Vector<Integer> Vector = new Vector<>();
+        ArrayList<Integer> list = new ArrayList<>(50000000);
         long time1 = System.currentTimeMillis();
         for(int i =0; i<50000000;i++){
-            List.add((int)(Math.random() * (Integer.MAX_VALUE + 1)));
+            list.add((int)(Math.random() * (Integer.MAX_VALUE + 1)));
         }
         long time2=System.currentTimeMillis();
-        long listTime = time2 - time1;
-        System.out.println("It took " + listTime + " milliseconds to put 50,000,000 elements in the array list");
+        long calcTime = time2 - time1;
+        System.out.println("It took " + calcTime + " milliseconds to put 50,000,000 elements in the array list");
 
-        long vectorTime1 = System.currentTimeMillis();
+        Vector<Integer> vector = new Vector<>(50000000);
+        time1 = System.currentTimeMillis();
         for(int i =0; i<50000000;i++){
-            Vector.add((int)(Math.random() * (Integer.MAX_VALUE + 1)));
+            vector.add((int)(Math.random() * (Integer.MAX_VALUE + 1)));
         }
-        long vectorTime2=System.currentTimeMillis();
-        long vectorTime = vectorTime2 - vectorTime1;
-        System.out.println("It took " + vectorTime + " milliseconds to put 50,000,000 elements in the vector");
+        time2=System.currentTimeMillis();
+        calcTime = time2 - time1;
+        System.out.println("It took " + calcTime + " milliseconds to put 50,000,000 elements in the vector");
 
-        long arrayTime1 = System.currentTimeMillis();
+        Integer[] array = new Integer[50000000];
+        time1 = System.currentTimeMillis();
         for(int i =0; i<50000000;i++){
-        Array[i] = ((int)(Math.random() * (Integer.MAX_VALUE + 1)));
+        array[i] = ((int)(Math.random() * (Integer.MAX_VALUE + 1)));
         }
-        long arrayTime2=System.currentTimeMillis();
-        long arrayTime = arrayTime2 - arrayTime1;
-        System.out.println("It took " + arrayTime + " milliseconds to put 50,000,000 elements in the array");
+        time2=System.currentTimeMillis();
+        calcTime = time2 - time1;
+        System.out.println("It took " + calcTime + " milliseconds to put 50,000,000 elements in the array");
 
+        time1 = System.currentTimeMillis();
+        Collections.sort(list);
+        time2 = System.currentTimeMillis();
+        calcTime = time2 - time1;
+        System.out.println("It took " + calcTime + " milliseconds to sort the Array List");
 
+        time1 = System.currentTimeMillis();
+        Collections.sort(vector);
+        time2 = System.currentTimeMillis();
+        calcTime = time2 - time1;
+        System.out.println("It took " + calcTime + " milliseconds to sort the Vector");
+
+        time1 = System.currentTimeMillis();
+        Arrays.sort(array);
+        time2 = System.currentTimeMillis();
+        calcTime = time2 - time1;
+        System.out.println("It took " + calcTime + " milliseconds to sort the Array");
     }
 }
